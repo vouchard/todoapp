83ee0eff14853aa29fb2241d2da83d58cdec9d8b
vouchard <vouchardorense@gmail.com>
2022-09-06 00:38:59 +0800

    todo items and comment model

--- todoitems/models.py
+++ todoitems/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class TodoItems(models.Model):
+    item = models.CharField(max_length=200,null=False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+class TodoComments(models.Model):
+    item = models.ForeignKey(TodoItems,on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200,null=False)
